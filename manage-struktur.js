b80757fc00d4732070dd2e0d5642d83753305f17
umann11002-web <umann11002@gmail.com>
2025-08-26 00:05:43 +0700

    Fitur: Struktur Organisasi rw

--- manage-struktur.js
+++ manage-struktur.js
@@ -14,7 +14,12 @@ import {
 
 // ... (Konfigurasi Firebase) ...
 const firebaseConfig = {
-  /* ... salin dari file lain ... */
+  apiKey: "AIzaSyBD4ypi0bq71tJfDdyqgdLL3A_RSye9Q7I",
+  authDomain: "rw16cibabat-dbf87.firebaseapp.com",
+  projectId: "rw16cibabat-dbf87",
+  storageBucket: "rw16cibabat-dbf87.appspot.com",
+  messagingSenderId: "744879659808",
+  appId: "1:744879659808:web:9d91c4bd2068260e189545",
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
